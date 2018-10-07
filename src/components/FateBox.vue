d291265b96f99ddb938183ea706ed1dd333cf0ed
pontakornth <pontakorn61@gmail.com>
2018-10-07 17:51:45 +0700

    First commit to dev

---
+++ src/components/FateBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+  <h1>
+    {{this.yourFate}}
+  </h1>
+  <button @click="yourFate = 0">I will see your fate</button>
+  </section>
+</template>
+
+<script>
+import {sample} from 'lodash-es'
+import fates from '../utils/fate_list'
+export default {
+  data: function () {
+    return {
+      result: sample(fates)
+    }
+  },
+  computed: {
+    yourFate: {
+      get () {
+        return this.result
+      },
+      set (value) {
+        this.result = sample(fates)
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/utils/fate_list.js
@@ -0,0 +1,10 @@
+let fates = [
+  'You will be promoted to a dog',
+  'Your wife/husband/bodyfriend/girlfirend will dump you!',
+  'You will earn lots of money',
+  'You won\'t have to work anymore because you will get ill',
+  'Your success is waitng, just do it',
+  'Anyone will like you from now on',
+  'OBJECTION!!'
+]
+export default fates
